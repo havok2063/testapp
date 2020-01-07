71bc3bfc426f9b516a7f124377544805e2903538
Brian Cherinka <havok2063@hotmail.com>
2020-01-07 10:39:09 -1000

    adding navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <v-app>
+    <v-navigation-drawer app v-model='drawer' dark>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Navigation
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>          
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="primary"
       dark
     >
       <div class="d-flex align-center">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-app-bar-nav-icon v-on="on" @click.stop="drawer = !drawer" />
+          </template>
+          <span>Toggle Navigation Drawer</span>
+        </v-tooltip>
+
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -54,7 +83,9 @@ export default {
   },
 
   data: () => ({
-    //
+    // template data goes here
+    drawer: null,
+    items: [{title: 'Stuff'}, {title:'Goes'}, {title:'Here'}]
   }),
 };
 </script>
